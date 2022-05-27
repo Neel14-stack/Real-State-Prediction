d9fd53f940a82c31d835e400e202f324cbb8fcf4
Neel Madhab <54886608+Neel14-stack@users.noreply.github.com>
2022-05-28 02:44:44 +0530

    update utlis.py file

--- server/utils.py
+++ server/utils.py
@@ -1,5 +1,6 @@
 import json
 import pickle
+import numpy as np
 
 __locations = None
 __data_columns = None
@@ -8,6 +9,24 @@ __model = None
 
 def get_location_names():
     return __locations
+
+def get_data_columns():
+    return __data_columns
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index>=0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0],2)
 
 
 def load_saved_artifacts():
@@ -26,6 +45,10 @@ def load_saved_artifacts():
     print("loading saved artifacts...done")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
